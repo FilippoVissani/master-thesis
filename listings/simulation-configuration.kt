a62df56c59838d476830825aede3ad87e99739fd
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-02-22 15:08:08 +0100

    docs: improve listing

--- listings/simulation-configuration.kt
+++ listings/simulation-configuration.kt
@@ -8,9 +8,9 @@ val environment = Environment.manhattanGrid(5, 5)
 
 val reactiveSensors = (0..<environment.devicesNumber).map {
     MutableStateFlow(
-        when {
-            it == 0 -> NodeType.SOURCE
-            it % 4 == 0 -> NodeType.OBSTACLE
+        when (it) {
+            0 -> NodeType.SOURCE
+            2, 7, 12 -> NodeType.OBSTACLE
             else -> NodeType.DEFAULT
         },
     )
